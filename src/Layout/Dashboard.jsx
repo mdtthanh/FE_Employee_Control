3c626c260b02b71544253c9bc11e1927a503a946
mdtthanh <maithanh0131@gmail.com>
2024-01-19 10:05:36 +0700

    fix bug

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -10,11 +10,9 @@ import List from '@material-ui/core/List';
 import Typography from '@material-ui/core/Typography';
 import Divider from '@material-ui/core/Divider';
 import IconButton from '@material-ui/core/IconButton';
-import Badge from '@material-ui/core/Badge';
 import MenuIcon from '@material-ui/icons/Menu';
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
-import NotificationsIcon from '@material-ui/icons/Notifications';
-import { mainListItems, secondaryListItems } from './listItems';
+import { mainListItems } from './listItems';
 import SimpleTable from './SimpleTable';
 
 const drawerWidth = 240;
@@ -141,9 +139,6 @@ class Dashboard extends React.Component {
               Dashboard
             </Typography>
             <IconButton color="inherit">
-              <Badge badgeContent={4} color="secondary">
-                <NotificationsIcon />
-              </Badge>
             </IconButton>
           </Toolbar>
         </AppBar>
--- src/Layout/SimpleTable.jsx
+++ src/Layout/SimpleTable.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
@@ -9,64 +9,64 @@ import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
 
 const styles = {
-  root: {
-    width: '100%',
-    overflowX: 'auto',
-  },
-  table: {
-    minWidth: 700,
-  },
+    root: {
+        width: '100%',
+        overflowX: 'auto',
+    },
+    table: {
+        minWidth: 700,
+    },
 };
 
 let id = 0;
-function createData(name, calories, fat, carbs, protein) {
-  id += 1;
-  return { id, name, calories, fat, carbs, protein };
-}
-
-const data = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-];
 
 function SimpleTable(props) {
-  const { classes } = props;
+    const { classes } = props;
+    const [data, setData] = useState([]);
 
-  return (
-    <Paper className={classes.root}>
-      <Table className={classes.table}>
-        <TableHead>
-          <TableRow>
-            <TableCell>Dessert (100g serving)</TableCell>
-            <TableCell align="right">Calories</TableCell>
-            <TableCell align="right">Fat (g)</TableCell>
-            <TableCell align="right">Carbs (g)</TableCell>
-            <TableCell align="right">Protein (g)</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {data.map(n => (
-            <TableRow key={n.id}>
-              <TableCell component="th" scope="row">
-                {n.name}
-              </TableCell>
-              <TableCell align="right">{n.calories}</TableCell>
-              <TableCell align="right">{n.fat}</TableCell>
-              <TableCell align="right">{n.carbs}</TableCell>
-              <TableCell align="right">{n.protein}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </Paper>
-  );
+    useEffect(() => {
+        fetch('http://localhost:5006/api/User/AllInfo')
+            .then(response => response.json())
+            .then(data => { 
+                console.log(data) 
+                setData(data)
+            })
+            .catch(error => console.error('Error:', error));
+    }, [])
+    return (
+        <Paper className={classes.root}>
+            <Table className={classes.table}>
+                <TableHead>
+                    <TableRow>
+                        <TableCell>順番</TableCell>
+                        <TableCell>Dessert (100g serving)</TableCell>
+                        <TableCell align="right">Calories</TableCell>
+                        <TableCell align="right">Fat (g)</TableCell>
+                        <TableCell align="right">Carbs (g)</TableCell>
+                        <TableCell align="right">Protein (g)</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {data.map((n, index) => (
+                        <TableRow key={n.id}>
+                            <TableCell>{index+1}</TableCell>
+                            <TableCell component="th" scope="row">
+                                {n.username}
+                            </TableCell>
+                            <TableCell align="right">{n.username}</TableCell>
+                            <TableCell align="right">{n.fat}</TableCell>
+                            <TableCell align="right">{n.carbs}</TableCell>
+                            <TableCell align="right">{n.protein}</TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        </Paper>
+    );
 }
 
 SimpleTable.propTypes = {
-  classes: PropTypes.object.isRequired,
+    classes: PropTypes.object.isRequired,
 };
 
 export default withStyles(styles)(SimpleTable);
