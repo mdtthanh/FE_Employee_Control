c83f50be4d762815e4becc150c2c362deb943df9
mdtthanh <maithanh0131@gmail.com>
2024-01-24 11:02:16 +0700

    edit form

--- src/Layout/SimpleTable.jsx
+++ src/Layout/SimpleTable.jsx
@@ -16,12 +16,16 @@ import Modal from '@mui/material/Modal';
 import Chip from '@mui/material/Chip';
 import { Close } from '@material-ui/icons';
 import TextField from '@mui/material/TextField';
+import MenuItem from '@mui/material/MenuItem';
 import dayjs, { Dayjs } from 'dayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker';
 import Autocomplete from '@mui/material/Autocomplete';
-
+import { TimePicker } from '@mui/x-date-pickers/TimePicker';
+import { DateField } from '@mui/x-date-pickers/DateField';
+import { TimeField } from '@mui/x-date-pickers/TimeField';
+import { DateTime } from 'luxon';
+import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker';
 const styles = {
     root: {
         width: '100%',
@@ -49,12 +53,13 @@ function SimpleTable(props) {
     const [data, setData] = useState([]);
     const [workPlace, setWorkPlace] = useState([]);
     const [username, setUsername] = useState('');
-    const [workTime, setWorkTime] = useState('');
+    const [workTime, setWorkTime] = useState(dayjs(''));
+    const [description, setDescription] = useState('');
     const [open, setOpen] = React.useState(false);
-    const [selectedWorkPlace, setSelectedWorkPlace] = useState(null);
+    const [selectedWorkPlace, setSelectedWorkPlace] = useState('');
     const [selectedWorkPlaceId, setSelectedWorkPlaceId] = useState(null);
     const [loading, setLoading] = useState(true);
-
+    const [idEmployee, setIdEmployee] = useState();
     const handleOpen = () => setOpen(true);
     const handleClose = () => setOpen(false);
 
@@ -67,13 +72,29 @@ function SimpleTable(props) {
     const handleUsernameChange = (event) => {
         setUsername(event.target.value);
     };
+    const handleDescriptionChange = (event) => {
+        setDescription(event.target.value);
+    };
+    const handleWorkTimeChange = (event) => {
+        console.log(workPlace)
+        setWorkTime(event.target.value);
+    };
+    const handleWorkplaceChange = (event, value) => {
+        // const selectedPlace = workPlace.find(place => place.workPlaceName === value);
+        // setSelectedWorkPlace(selectedPlace);
+        // setSelectedWorkPlaceId(selectedPlace ? selectedPlace.id : null);
+        // setSelectedWorkPlace(event.target.value);
+        // console.log(value)   
+    }
 
     const handleSubmit = (event) => {
         event.preventDefault();
         // const data = new FormData(event.target);
         const info = JSON.stringify({
             "username": username,
-            "workPlaceId": selectedWorkPlaceId
+            "workPlaceId": selectedWorkPlaceId,
+            "description": description,
+            // "workTime": workTime
         })
         fetch('http://localhost:5006/api/User/AddAllInfo', {
             method: 'POST',
@@ -84,15 +105,60 @@ function SimpleTable(props) {
         })
             .then(response => response.json())
             .then(data => {
-                console.log(data);
                 setData(prevData => [...prevData, data]);
-                setOpen(false);
                 window.location.reload()
+                setOpen(false);
 
             })
             .catch((error) => console.error('Error:', error));
     };
 
+    const handleShowInfoEmployee = (id) => {
+        fetch(`http://localhost:5006/api/User/AllInfo/${id}`)
+            .then(response => response.json())
+            .then(data => {
+                // Set the state with the fetched data
+                setUsername(data.username);
+                setSelectedWorkPlaceId(data.workPlaceId);
+                setDescription(data.description);
+                setSelectedWorkPlace(data.workPlace.workPlaceName)
+                setIdEmployee(data.id);
+                setWorkTime(data.workTime);
+
+                // Open the edit dialog
+                setOpen(true);
+                console.log(data.workTime)
+                // console.log(data)
+            })
+            .catch((error) => console.error('Error:', error));
+    };
+
+    const handleEdit = (id) => {
+        var myHeaders = new Headers();
+        myHeaders.append("Content-Type", "application/json");
+
+        var raw = JSON.stringify({
+            "username": username,
+            "workPlaceId": selectedWorkPlaceId,
+            "description": description,
+            "workTime": workTime
+        });
+
+        var requestOptions = {
+            method: 'PUT',
+            headers: myHeaders,
+            body: raw,
+            redirect: 'follow'
+        };
+        fetch(`http://localhost:5006/api/User/EditInfo/${id}`, requestOptions)
+            .then(response => response.json())
+            .then(result => {
+                setOpen(false);
+                window.location.reload()
+                console.log(result)
+            })
+            .catch(error => console.log('error', error));
+    }
     const handleDelete = (id) => {
         fetch(`http://localhost:5006/api/User/DeleteInfo/${id}`, {
             method: 'DELETE',
@@ -125,7 +191,6 @@ function SimpleTable(props) {
             .catch(error => console.error('Error:', error));
     }, [])
 
-    const workPlaceNames = workPlace.map(place => place.workPlaceName);
     return (
         <>
             <Button
@@ -150,46 +215,42 @@ function SimpleTable(props) {
                         </TableRow>
                     </TableHead>
                     <TableBody>
-                        {data.map((n, index) => (
-                            <TableRow key={n.id}>
-                                <TableCell>{index + 1}</TableCell>
-                                <TableCell component="th" align="left" scope="row">
-                                    {n.username}
-                                </TableCell>
-                                <TableCell align="left">{n.workTime}</TableCell>
-                                <TableCell align="center">
-                                    <Chip
-                                        sx={{
-                                            "width": "100px"
-                                        }}
-                                        label={`${n.workPlace ? n.workPlace.workPlaceName : 'N/A'}`}
-                                        color="success" />
-                                </TableCell>
-                                <TableCell align="left">{n.description}</TableCell>
-                                <TableCell align="center">
-                                    <EditIcon
-                                        onClick={handleOpen}
-                                        className='button_edit'
-                                        sx={{
-                                            "cursor": "pointer",
-                                            ":hover": {
-                                                color: "blue"
-                                            }
-                                        }}
-                                    />
-                                    <DeleteIcon
-                                        onClick={() => handleDelete(n.id)}
-                                        className='button_edit'
-                                        sx={{
-                                            "cursor": "pointer",
-                                            ":hover": {
-                                                color: "red"
-                                            }
-                                        }}
-                                    />
-                                </TableCell>
-                            </TableRow>
-                        ))}
+                        {data.map((n, index) => {
+                            // Convert workTime to a Date object
+                            const date = new Date(n.workTime);
+                            // Format the hours and minutes
+                            const hours = date.getHours();
+                            const minutes = date.getMinutes();
+                            const formattedWorkTime = `${hours}:${minutes < 10 ? '0' + minutes : minutes}`;
+
+                            return (
+                                <TableRow key={n.id}>
+                                    <TableCell>{index + 1}</TableCell>
+                                    <TableCell component="th" align="left" scope="row">
+                                        {n.username}
+                                    </TableCell>
+                                    <TableCell align="left">{formattedWorkTime}</TableCell>
+                                    <TableCell align="center">
+                                        <Chip
+                                            sx={{ "width": "100px" }}
+                                            label={`${n.workPlace ? n.workPlace.workPlaceName : 'N/A'}`}
+                                            color="success" />
+                                    </TableCell>
+                                    <TableCell align="left">{n.description}</TableCell>
+                                    <TableCell align="center">
+                                        <EditIcon
+                                            onClick={() => handleShowInfoEmployee(n.id)}
+                                            className='button_edit'
+                                            sx={{ "cursor": "pointer", ":hover": { color: "blue" } }}
+                                        />
+                                        <DeleteIcon
+                                            sx={{ "cursor": "pointer", ":hover": { color: "red" } }}
+                                            onClick={() => handleDelete(n.id)}
+                                        />
+                                    </TableCell>
+                                </TableRow>
+                            );
+                        })}
                     </TableBody>
                 </Table>
             </Paper>
@@ -236,26 +297,32 @@ function SimpleTable(props) {
                                 <LocalizationProvider dateAdapter={AdapterDayjs}>
                                     <DateTimePicker
                                         label="Controlled picker"
-                                        defaultValue={dayjs('2022-04-17T15:30')}
                                         sx={{
                                             "marginTop": "20px",
                                             "width": "415px"
                                         }}
+                                        value={dayjs(workTime)}
+                                        onChange={(selectedDate) => setWorkTime(selectedDate)}
                                     />
                                 </LocalizationProvider>
                             </Typography>
                             <Typography >
-                                <Autocomplete
-                                    disablePortal
-                                    id="combo-box-demo"
-                                    options={workPlaceNames}
+                                <TextField
+                                    label="Location"
+                                    value={selectedWorkPlaceId}
+                                    onChange={(e) => setSelectedWorkPlaceId(e.target.value)}
+                                    select
                                     sx={{
                                         "marginTop": "20px",
                                         "width": "415px"
                                     }}
-                                    onChange={handleChange}
-                                    renderInput={(params) => <TextField {...params} label="Location" />}
-                                />
+                                >
+                                    {workPlace.map((place) => (
+                                        <MenuItem value={place.id}>
+                                            {place.workPlaceName}
+                                        </MenuItem>
+                                    ))}
+                                </TextField>
                             </Typography>
                             <Typography>
                                 <TextField
@@ -266,6 +333,8 @@ function SimpleTable(props) {
                                         "marginTop": "20px",
                                         "width": "415px"
                                     }}
+                                    value={description}
+                                    onChange={handleDescriptionChange}
                                 />
                             </Typography>
 
@@ -284,7 +353,7 @@ function SimpleTable(props) {
                                 sx={{
                                     marginRight: '10px'
                                 }}
-                                onClick={handleSubmit}
+                                onClick={() => handleEdit(idEmployee)}
                             >
                                 Save
                             </Button>
