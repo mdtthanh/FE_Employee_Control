032694051c286225e4792295bada7f37e3b5bdff
mdtthanh <maithanh0131@gmail.com>
2024-01-26 17:50:56 +0700

    add

--- src/Layout/SimpleTable.jsx
+++ src/Layout/SimpleTable.jsx
@@ -21,15 +21,10 @@ import dayjs from "dayjs";
 import utc from 'dayjs-plugin-utc';
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
-import Autocomplete from "@mui/material/Autocomplete";
-import { TimePicker } from "@mui/x-date-pickers/TimePicker";
-import { DateField } from "@mui/x-date-pickers/DateField";
-import { TimeField } from "@mui/x-date-pickers/TimeField";
-import { DateTime } from "luxon";
 import { DateTimePicker } from "@mui/x-date-pickers/DateTimePicker";
-import { formatToTimeZone } from "date-fns-timezone";
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import Grid from '@mui/material/Grid';
+import 'dayjs/locale/zh-cn';
 dayjs.extend(utc);
 const styles = {
     root: {
@@ -57,6 +52,7 @@ const statusColors = ["#7986cb", "#2196f3", "#4fc3f7", "#4dd0e1", "#4db6ac", "#8
 function SimpleTable(props) {
     const { classes } = props;
     const [data, setData] = useState([]);
+
     const [workPlace, setWorkPlace] = useState([]);
     const [username, setUsername] = useState("");
     const [workTime, setWorkTime] = useState("");
@@ -68,7 +64,8 @@ function SimpleTable(props) {
     const [idEmployee, setIdEmployee] = useState();
     const handleOpen = () => setOpen(true);
     const handleClose = () => setOpen(false);
-    const [selectedDate, setSelectedDate] = useState(null);
+    const [selectedDate, setSelectedDate] = useState(new Date('2024-01-01'));
+    const [currentDate, setCurrentDate] = useState(new Date());
     // const [filteredData, setFilteredData] = useState(null);
     const [matchingEmployees, setMatchingEmployees] = useState(data);
     function padTo2Digits(num) {
@@ -79,27 +76,52 @@ function SimpleTable(props) {
         return String(num).padStart(2, "0");
     }
 
-    
-    function handleDateChange(date) {
-        const dateStr = new Date(date)
-        const day = dateStr.getDate()-1; // get the day
-        const month = dateStr.getMonth() + 1; // get the month (getMonth() returns month index starting from 0)
-        const year = dateStr.getFullYear(); // get the year
-        // Convert the selected date to a string in the format YYYY-MM-DD
-        const timeDate = `${padTo2Digits(day)}/${padTo2Digits(month)}/${year}`;
+    //     function handleDateChange(date) {
+    //         const dateStr = new Date(date)
+    //         const day = dateStr.getDate()-1; // get the day
+    //         const month = dateStr.getMonth() + 1; // get the month (getMonth() returns month index starting from 0)
+    //         const year = dateStr.getFullYear(); // get the year
+    //         // Convert the selected date to a string in the format YYYY-MM-DD
+    //         const timeDate = `${padTo2Digits(day)}/${padTo2Digits(month)}/${year}`;
 
-        // Filter the employees whose workTime matches the selected date
-        const matchingEmployees = data.filter(employee => {
-            // Convert the employee's workTime to a date and then to a string in the format YYYY-MM-DD
-            const employeeDate = new Date(employee.workTime);
-            const formattedWorkTime = employeeDate.toLocaleDateString('en-GB', { timeZone: 'UTC' });
-            // console.log(formattedWorkTime)
-            return formattedWorkTime === timeDate;
-        });
-        setMatchingEmployees(matchingEmployees);
-        // console.log(matchingEmployees)
-        // console.log(timeDate);
-    }
+    //         // Filter the employees whose workTime matches the selected date
+    //         const matchingEmployees = data.filter(employee => {
+    //             // Convert the employee's workTime to a date and then to a string in the format YYYY-MM-DD
+    //             const employeeDate = new Date(employee.workTime);
+    //             const formattedWorkTime = employeeDate.toLocaleDateString('en-GB', { timeZone: 'UTC' });
+    //             // console.log(formattedWorkTime)
+    //             return formattedWorkTime === timeDate;
+    //         });
+    //         setMatchingEmployees(matchingEmployees);
+    //         // console.log(matchingEmployees)
+    //         // console.log(timeDate);
+    //         const today = new Date();
+    //         const date1 = today.getDate(); // Ngày của tháng (từ 1 đến 31)
+    //         const month1 = today.getMonth() + 1; // Tháng (từ 0 đến 11, nên cần cộng thêm 1 để có tháng từ 1 đến 12)
+    //         const year1= today.getFullYear(); // Năm
+
+    // // console.log(`Hôm nay là ngày ${date1} tháng ${month1} năm ${year1}`);
+    //     }
+
+    const handleDateChange = (date) => {
+        setSelectedDate(date);
+        const dateStr = new Date(date)
+        const day = dateStr.getDate() - 1;
+        const month = dateStr.getMonth() + 1; // getMonth() returns a value from 0 to 11
+        const year = dateStr.getFullYear();
+
+        fetch(`http://localhost:5006/api/User/DateNow/${day}/${month}/${year}`)
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                // Handle the response data
+                setMatchingEmployees(data);
+            })
+            .catch(error => {
+                console.error("Error:", error);
+                // Handle the error
+            });
+    };
 
     const handleAddEmployeeClick = () => {
         setIdEmployee('');
@@ -128,13 +150,6 @@ function SimpleTable(props) {
     const handleWorkTimeChange = (event) => {
         console.log(workPlace);
         setWorkTime(event.target.value);
-    };
-    const handleWorkplaceChange = (event, value) => {
-        // const selectedPlace = workPlace.find(place => place.workPlaceName === value);
-        // setSelectedWorkPlace(selectedPlace);
-        // setSelectedWorkPlaceId(selectedPlace ? selectedPlace.id : null);
-        // setSelectedWorkPlace(event.target.value);
-        // console.log(value)
     };
 
     const handleSubmit = (event) => {
@@ -208,6 +223,15 @@ function SimpleTable(props) {
             })
             .catch((error) => console.log("error", error));
     };
+    useEffect(() => {
+        fetch("http://localhost:5006/api/User/DateNow")
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data);
+                setData(data);
+            })
+            .catch((error) => console.error("Error:", error));
+    }, []);
     const handleDelete = (id) => {
         fetch(`http://localhost:5006/api/User/DeleteInfo/${id}`, {
             method: "DELETE",
@@ -223,15 +247,16 @@ function SimpleTable(props) {
     useEffect(() => {
         setMatchingEmployees(data);
     }, [data]);
-    useEffect(() => {
-        fetch("http://localhost:5006/api/User/AllInfo")
-            .then((response) => response.json())
-            .then((data) => {
-                console.log(data);
-                setData(data);
-            })
-            .catch((error) => console.error("Error:", error));
-    }, []);
+    // useEffect(() => {
+    //     fetch("http://localhost:5006/api/User/AllInfo")
+    //         .then((response) => response.json())
+    //         .then((data) => {
+    //             console.log(data);
+    //             setData(data);
+    //         })
+    //         .catch((error) => console.error("Error:", error));
+    // }, []);
+
 
     useEffect(() => {
         fetch("http://localhost:5006/api/User/AllInfoWorkPlace")
@@ -244,11 +269,14 @@ function SimpleTable(props) {
     }, []);
     return (
         <>
+
             <Grid sx={{ display: "flex", justifyContent: "end" }}>
                 <LocalizationProvider dateAdapter={AdapterDayjs}>
                     <DatePicker
-                        label="免社時間"
-                        value={selectedDate}
+                        label="帰社時間"
+                        value={dayjs(currentDate)}
+                        format="YYYY/MM/DD"
+                        // defaultValue={dayjs(currentDate)}
                         // onChange={(newValue) => setWorkTime(newValue)}
                         onChange={(newValue) => {
                             // const formattedDate = dayjs(newValue).format('DD:MM:YYYY');
@@ -305,7 +333,7 @@ function SimpleTable(props) {
                             </TableCell>
                             <TableCell align="center">
                                 <Typography sx={{ fontSize: "20px", fontWeight: "bold" }}>
-                                    免社時間
+                                    帰社時間
                                 </Typography>
                             </TableCell>
                             <TableCell align="center" width={"30%"}>
@@ -344,7 +372,7 @@ function SimpleTable(props) {
                                         <Chip
                                             sx={{
                                                 width: "100px",
-                                                backgroundColor: statusColors[n.workPlace.id - 1 ],
+                                                backgroundColor: statusColors[n.workPlace.id - 1],
                                                 color: "black",
                                                 fontWeight: "bold",
                                                 fontSize: "18px"
@@ -429,8 +457,8 @@ function SimpleTable(props) {
                                         required
                                         inputFormat="dd/MM/YYYY 
                                         HH:mm"
-                                        
-                                        label="免社時間"
+
+                                        label="帰社時間"
                                         sx={{
                                             marginTop: "20px",
                                             width: "415px",
